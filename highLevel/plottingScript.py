5aca53263b26d72fb67c511d6eb4b1b28d503550
Mark <movermye@its.jnj.com>
2017-07-22 13:55:37 -0400

    basic scrips to plot streaming data begun

---
+++ highLevel/plottingScript.py
@@ -0,0 +1,24 @@
+#some attempts to learn plotting with python
+#ideally, this will feed into the ability
+#to make a scrolling plot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.ion()
+
+for i in range(1000):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.pause(0.05)
+
+    minX = max(0, i-10)
+
+    plt.axis([minX, i+1, 0, 1])
+
+while True:
+    plt.pause(0.05)
+
+
+
